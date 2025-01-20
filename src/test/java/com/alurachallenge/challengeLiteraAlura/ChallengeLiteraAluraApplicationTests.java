42837d96ab126bc8807b7de9bce97549f0bad51a
Jcmoralesl <juancamilo.morales43@gmail.com>
2025-01-19 21:25:06 -0500

    Inicialización del proyecto

---
+++ src/test/java/com/alurachallenge/challengeLiteraAlura/ChallengeLiteraAluraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alurachallenge.challengeLiteraAlura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeLiteraAluraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
